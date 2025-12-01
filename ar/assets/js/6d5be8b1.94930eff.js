88f8af4be7e097e5f2e50b6292e65b2ba6dab88e
pvega62 <vega.pedro96@gmail.com>
2025-12-01 18:52:18 -0500

    added arabic translation

---
+++ ar/assets/js/6d5be8b1.94930eff.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocusaurus=globalThis.webpackChunkdocusaurus||[]).push([[7712],{4502:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/software/ar/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"\u0627\u0644\u0645\u062f\u0648\u0646\u0629","blogTitle":"\u0627\u0644\u0645\u062f\u0648\u0646\u0629"}}')}}]);
---
+++ ar/assets/js/acecf23e.a2558c28.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocusaurus=globalThis.webpackChunkdocusaurus||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/software/ar/blog","blogTitle":"Blog","authorsListPath":"/software/ar/blog/authors"}')}}]);
